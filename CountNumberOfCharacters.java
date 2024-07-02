b9c186ae223aa4618d6ecce85b9096ae6a1a072a
Abhi Nivesh R <abhi-21739@abhi-21739.csez.zohocorpin.com>
2024-07-02 14:35:48 +0530

    Added CountNumberOfCharacters program

---
+++ CountNumberOfCharacters.java
@@ -0,0 +1,26 @@
+//Write a program to count the numbers of characters entered through stdin. The program exits upon entering Ctrl+Z
+//in mac ctrl+D
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountNumberOfCharacter{
+    public static void main(String[] arg){
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        int count = 0;
+        try{
+            int character;
+            while((character=reader.read())!=-1){
+                count++;
+                System.out.println(character+" ");
+            }
+            
+        }
+        catch(IOException e){
+            System.out.println("Error "+e);
+        }
+        System.out.println("Number of characters entered : "+count);
+    }
+}
+
