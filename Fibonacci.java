1eafc6a2de263f4a4be4d904060796fbd2e689c8
Abhi Nivesh R <abhi-21739@abhi-21739.csez.zohocorpin.com>
2024-07-04 12:58:08 +0530

    Added Fibonacci program

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,6 @@
-//Program to print Fibonacci series of a number using recursion. Get the number through command line argument. Throw proper errors for wrong inputs.
+// Program to print Fibonacci series of a number using recursion. Get the number through command line argument. Throw proper errors for wrong inputs.
 // 0 1 1 2 3
+
 import java.util.Scanner;
 
 public class Fibonacci{
@@ -10,16 +11,6 @@ public class Fibonacci{
         return fib(n-1)+fib(n-2);
         
     }
-    // static int a=0,b=1,c=0;
-    // public static void fib1(int n){
-    //     if(n>0){
-    //         c=a+b;
-    //         a=b;
-    //         b=c;
-    //         System.out.print(c+" ");
-    //         fib1(n-1);
-    //     }
-    // }
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the value : ");
@@ -34,7 +25,6 @@ public class Fibonacci{
             return;
         }
         System.out.print("Fibonacci up to "+n+" : ");
-        //fib1(n);
         for(int i=0;i<=n;i++){
             System.out.print(fib(i)+" ");
         }
