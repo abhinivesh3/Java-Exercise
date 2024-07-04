0b9d2d0555bdfb77787ef3c3b44a685ef344c21d
Abhi Nivesh R <abhi-21739@abhi-21739.csez.zohocorpin.com>
2024-07-04 12:59:31 +0530

    Added 6 - FileCopy program

--- FileCopy.java
+++ FileCopy.java
@@ -6,11 +6,22 @@ import java.io.IOException;
 
 public class FileCopy {
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java FileCopy <source_file> <destination_file>");
+            System.exit(1);
+        }
+
+        String sourceFile = args[0];
+        String destinationFile = args[1];
+
         FileInputStream fileInputStream=null;
         FileOutputStream fileOutputStream=null;
         try{
-            fileInputStream=new FileInputStream("/Users/abhi-21739/Documents/programFileStream/inputFile.txt");
-            fileOutputStream=new FileOutputStream("/Users/abhi-21739/Documents/programFileStream/outputFile.txt");
+            // fileInputStream=new FileInputStream("/Users/abhi-21739/Documents/programFileStream/inputFile.txt");
+            // fileOutputStream=new FileOutputStream("/Users/abhi-21739/Documents/programFileStream/outputFile.txt");
+            
+            fileInputStream=new FileInputStream(sourceFile);
+            fileOutputStream=new FileOutputStream(destinationFile);
             
             int c;
             while((c=fileInputStream.read())!=-1){
